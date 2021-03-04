a4674a6c2825461e2d1a5fab616f1748087e2ab0
Jordan <50714347+jescarraga@users.noreply.github.com>
2021-03-03 21:47:28 -0500

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
+int main() {
+    int casos;
+    printf("ingresa un 1 o un 2: \n");
+    scanf("%d",&casos);
+    switch(casos){
+      case 1:
+             printf("elegiste el primer caso");
+             break;
+      case 2:
+             printf("elegiste el caso 2");
+             break;
+    }
 }
