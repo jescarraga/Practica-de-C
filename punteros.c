f770ded7cd14b1c20220c922e6379f279804892c
JORDAN <jescarraga@unal.edu.co>
2021-03-03 23:16:50 -0500

    Punteros actualizados

--- punteros.c
+++ punteros.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void cubo (int *n){
+    *n = *n * *n * *n;
+}
 
 int main() {
     //PUNTEROS BASICOS
@@ -10,5 +13,14 @@ int main() {
     // imprimimos el valor al que el apuntador apt apunta
     printf("%i \n",*apt);
     // imprimimosla dirección de memoria a la que apunta apt
-    printf("%i \n",apt);
+    printf("%p \n",apt);
+
+
+
+    //Funcion por referencia
+    int num = 2;
+    printf("numero actual %i \n",num);
+    cubo(&num);
+    printf("numero al cubo %i \n",num);
+
 }
